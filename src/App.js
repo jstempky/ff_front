5cd320f10e81e177ad37c292aa752dfec6d861a9
Joey Stempky <joeystempky@Joeys-MacBook-Air.local>
2019-08-08 17:05:34 -0400

    start of styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import "./App.css";
 class App extends Component {
   render() {
     if (window.location.pathname === "/") {
-      return <div>Hello World</div>;
+      return (
+        <div>
+          <h1>Hello Joey</h1>
+          <h3>To Do's:</h3>
+          <ul>
+            <li>Register</li>
+            <li>Login/Authentication</li>
+            <li>Dashboard for User</li>
+          </ul>
+        </div>
+    );
     } else {
       return <div />;
     }
--- src/Reports/Dashboard.js
+++ src/Reports/Dashboard.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
-import logo from "../logo.svg";
 import { withRouter } from "react-router-dom";
+
+import "../App.css"
+
+import icon_data from "../resources/icon_data.png";
+import icon_History from "../resources/icon_History.png";
+import icon_Me from "../resources/icon_Me.png";
+import icon_Records from "../resources/icon_Records.png";
+import icon_NewSet from "../resources/icon_NewSet.png";
+import icon_Social from "../resources/icon_Social.png";
+import logo from "../resources/logo.png";
 
 class Dashboard extends Component {
   navigateToGraphs = () => {
@@ -28,16 +37,70 @@ class Dashboard extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <img onClick={this.navigateToGraphs} src={logo} />
-        <img onClick={this.navigateToHistory} src={logo} />
-        <img onClick={this.navigateToAccount} src={logo} />
-        <img onClick={this.navigateToNewSet} src={logo} />
-        <img onClick={this.navigateToMaxReps} src={logo} />
-        <img onClick={this.navigateToSocial} src={logo} />
-      </div>
-    );
+    if (window.location.pathname==="/dashboard") {
+      return (
+          <div className="dashboardStyle">
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToGraphDash}
+                src={icon_data}
+              />
+              <figcaption className="iconTitle">Graphs</figcaption>
+            </figure>
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToHistory}
+                src={icon_History}
+              />
+              <figcaption
+                className="iconTitle"
+              >Historical</figcaption>
+            </figure>
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToAccount}
+                src={icon_Me} />
+              <figcaption
+                className="iconTitle"
+              >Me</figcaption>
+            </figure>
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToNewSet}
+                src={icon_NewSet} />
+              <figcaption
+                className="iconTitle"
+              >New Set</figcaption>
+            </figure>
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToMaxReps}
+                src={icon_Records} />
+              <figcaption
+                className="iconTitle"
+              >Records</figcaption>
+            </figure>
+            <figure>
+              <img
+                className="dash_icon"
+                onClick={this.navigateToSocial}
+                src={icon_Social} />
+              <figcaption
+                className="iconTitle"
+              >Social</figcaption>
+            </figure>
+          </div>
+        )
+      }
+      else {
+        return <div />;
+    }
+    }
   }
-}
+
 export default withRouter(Dashboard);
